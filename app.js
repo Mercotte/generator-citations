f4f9af6a68da3c0bf9eb3711e5a8d089114c2c82
Mercotte <sbhpro@outlook.fr>
2023-05-31 12:12:47 +0200

    modification css/js

--- app.js
+++ app.js
@@ -9,21 +9,24 @@ const quotes = [
   "La théorie, c'est quand on sait tout et que rien ne fonctionne. La pratique, c'est quand tout fonctionne et que personne ne sait pourquoi. Ici, nous avons réuni théorie et pratique : Rien ne fonctionne... et personne ne sait pourquoi !",
   'Le langage de programmation que vous appréciez le plus est généralement celui dans lequel vous étiez le plus compétent.',
 ];
-const btn = document.querySelector('.btn');
 
+const btn = document.querySelector('.btn');
 const text = document.querySelector('.text');
 const faSolid = document.querySelector('.fa-solid');
 
 btn.addEventListener('click', function () {
-  let color = ['red', 'blue', 'green', 'yellow'];
-  let valeurCouleur = color[getRandomInt(color.length)];
-  let valeurCitation = quotes[getRandomInt(quotes.length)];
-  btn.style.background = valeurCouleur;
-  faSolid.style.color = valeurCouleur;
-  document.body.style.background = valeurCouleur;
+  let valeur1 = getRandomInt();
+  let valeurCitation = quotes[getRandomInt1(quotes.length)];
+  btn.style.background = '#' + valeur1;
+  faSolid.style.color = '#' + valeur1;
+  document.body.style.background = '#' + valeur1;
   text.textContent = valeurCitation;
+  console.log(valeurCitation);
 });
 
-function getRandomInt(max) {
+function getRandomInt1(max) {
   return Math.floor(Math.random() * max);
+}
+function getRandomInt() {
+  return Math.floor(Math.random() * 16777215).toString(16);
 }
